2004dead1a55557d4c51b21d9f0bc3e66d06cc7d
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-10-19 15:02:59 +0100

    Start Menu Done
    Start menu done

---
+++ AIsDefence/Assets/Scripts/Menus/mainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+    public string TestScene;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(TestScene, LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Button Has Been Pressed!");
+    }
+}
