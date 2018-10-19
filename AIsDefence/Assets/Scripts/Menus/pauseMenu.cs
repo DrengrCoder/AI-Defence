60dab87f5a88a8fa5afd68dfd13c1221805e7c6f
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-10-19 14:50:47 +0100

    esc menu done

--- AIsDefence/Assets/Scripts/Menus/pauseMenu.cs
+++ AIsDefence/Assets/Scripts/Menus/pauseMenu.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class pauseMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public GameObject ESCMenu;
+
+    private bool _on = false;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            SwitchState();
+        }
+    }
+
+    public void SwitchState()
+    {
+        if (_on == true)
+        {
+            ESCMenu.SetActive(true);
+            Time.timeScale = 0;
+            _on = false;
+        }
+        else
+        {
+            ESCMenu.SetActive(false);
+            Time.timeScale = 1;
+            _on = true;
+        }
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit Button Has Been Pressed!");
+    }
+
 }
