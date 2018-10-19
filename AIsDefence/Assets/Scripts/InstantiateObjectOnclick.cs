815b4b47b1babf26c65ca8c56d09aa82db073e18
dylan mcadam <dylan@dylanmcadam.co.uk>
2018-10-19 17:15:08 +0100

    TowerSelection script alternates button selection for different towers;; InstantiateObjectOnclick script will create a game object prefab at the mouse pointer position when called;; Added two enemy prefabs and coloured them for the instantiate object script

---
+++ AIsDefence/Assets/Scripts/InstantiateObjectOnclick.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateObjectOnclick : MonoBehaviour {
+
+    private Ray ray;
+    private RaycastHit hit;
+
+    public GameObject redPrefab;
+    public GameObject blackPrefab;
+
+    private bool pressedDelay = false;
+
+    private TowerSelection towerSelection;
+
+	// Use this for initialization
+	void Start () {
+		this.towerSelection = GameObject.Find("Canvas").GetComponent<TowerSelection>();
+        Debug.Log(this.towerSelection == null);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (this.pressedDelay == false && (this.towerSelection.RedTowerSelected() || this.towerSelection.BlackTowerSelected())) {
+                if (Input.GetKey(KeyCode.Mouse0))
+                {
+                    pressedDelay = true;
+
+                    if (this.towerSelection.RedTowerSelected())
+                    {
+                        GameObject obj = Instantiate(redPrefab, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
+                    }
+                    else if (this.towerSelection.BlackTowerSelected())
+                    {
+                        GameObject obj = Instantiate(blackPrefab, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
+                    }
+
+                    StartCoroutine(ResetDelay());
+                }
+            }
+
+        }
+    }
+
+    IEnumerator ResetDelay()
+    {
+        yield return new WaitForSeconds(1);
+        pressedDelay = false;
+    }
+}
---
+++ AIsDefence/Assets/Scripts/TowerSelection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerSelection : MonoBehaviour {
+    
+    public Button[] BUTTONS;
+
+    private bool blackPressed = false;
+    private bool redPressed = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.BUTTONS[0].GetComponent<Button>().onClick.AddListener(delegate { ButtonClick(0); });
+        this.BUTTONS[1].GetComponent<Button>().onClick.AddListener(delegate { ButtonClick(1); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ButtonClick(int i)
+    {
+        this.BUTTONS[0].GetComponent<Button>().GetComponent<Image>().color = new Color(255, 255, 255);
+        this.BUTTONS[1].GetComponent<Button>().GetComponent<Image>().color = new Color(255, 255, 255);
+        this.blackPressed = false;
+        this.redPressed = false;
+
+        switch (i)
+        {
+            case 0:
+                this.BUTTONS[0].GetComponent<Button>().GetComponent<Image>().color = new Color(0, 255, 0);
+                this.blackPressed = true;
+                break;
+            case 1:
+                this.BUTTONS[1].GetComponent<Button>().GetComponent<Image>().color = new Color(0, 255, 0);
+                this.redPressed = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public bool RedTowerSelected()
+    {
+        return this.redPressed;
+    }
+
+    public bool BlackTowerSelected()
+    {
+        return this.blackPressed;
+    }
+}
