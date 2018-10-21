47c095600206c339036c3e13c0d71a4359cd5440
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-10-21 11:28:27 +0100

    Enemy outline
    Started the sprint, enemies move.  Outline of the rest done.

---
+++ AIsDefence/Assets/Scripts/Enemies/EnemyManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] _spawnableEnemies;
+    [SerializeField]
+    private int[] _maxEnemiesOfType;
+    [SerializeField]
+    private GameObject _spawnPoint;
+
+    private int[] _spawnedEnemies;
+    private float _spawnDelay = 10.0f;
+
+    private void SpawnEnemy()
+    {
+
+    }
+
+    private bool CheckEnemyNum()
+    {
+        return false;
+    }
+
+}
---
+++ AIsDefence/Assets/Scripts/Enemies/SuicideEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuicideEnemy : Enemy {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject == EnemyTarget)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attack");
+        Death();
+    }
+
+}
