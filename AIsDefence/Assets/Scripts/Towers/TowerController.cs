3581b7457a732b219d51c7d87b9f0e09fa25278c
dylan mcadam <dylan@dylanmcadam.co.uk>
2018-10-25 17:03:50 +0100

    shooting bullets when enemies trigger sphere colliders on tower prefabs (fires directly parallel to z-axis)

---
+++ AIsDefence/Assets/Scripts/Towers/TowerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _bullet;
+
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            GameObject firedBullet = Instantiate(this._bullet, transform.position, Quaternion.identity) as GameObject;
+            firedBullet.GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+        }
+    }
+}
