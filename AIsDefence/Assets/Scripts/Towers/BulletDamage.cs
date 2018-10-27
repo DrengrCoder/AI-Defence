779b22ae192139b499d7b7f7cb0c25b951df6517
dylan mcadam <dylan@dylanmcadam.co.uk>
2018-10-27 15:17:42 +0100

    created 'bullet damage' script to 'kill' enemies on hit, bomb aoe damage kills multiple enemies in a radius.

---
+++ AIsDefence/Assets/Scripts/Towers/BulletDamage.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (!obj.isTrigger && obj.tag == "Enemy")
+        {
+            if (this.gameObject.name.Contains("Bullet"))
+            {
+                obj.gameObject.SetActive(false);
+            }
+            else if (this.gameObject.name.Contains("Bomb"))
+            {
+                Transform bombChild = this.gameObject.transform.GetChild(0);
+                
+                Vector3 vect = new Vector3(bombChild.position.x, bombChild.position.y, bombChild.position.z);
+
+                Collider[] detectedColliders = Physics.OverlapSphere(vect, 0.1f);
+
+                foreach (Collider col in detectedColliders)
+                {
+                    if (col.gameObject.tag == "Enemy")
+                    {
+                        col.gameObject.SetActive(false);
+                    }
+                }
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+}
