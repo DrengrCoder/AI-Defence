6358380e6b0a6ad03953b39b3df874eaf0667d36
dylan mcadam <dylan@dylanmcadam.co.uk>
2018-10-28 13:46:05 +0000

    Created script to toggle an edit menu on and off

---
+++ AIsDefence/Assets/Scripts/Menus/EditTowerMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditTowerMenu : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _editMenu;
+
+    private bool _on = false;
+    	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleMenu();
+        }
+	}
+
+    private void ToggleMenu()
+    {
+        if (!_on)
+        {
+            this._editMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            this._editMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+        _on = !_on;
+    }
+}
