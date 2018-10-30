2d4ac7eea7ababd1bd79addf944456de314e1612
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-10-30 16:36:59 +0000

    Simple Player Controller done
    Can swap to it using P
    Error found with melee enemies.

---
+++ AIsDefence/Assets/Scripts/Player/ActivatePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePlayer : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _character;
+
+    private string _button = "PlayCharacter";
+    private bool _active = false;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown(_button))
+        {
+            _active = !_active;
+            _character.SetActive(_active);
+        }
+    }
+}
