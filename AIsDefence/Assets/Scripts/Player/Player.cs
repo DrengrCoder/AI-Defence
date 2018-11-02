1080a1bf783da6ec85bc992544fb67184e257ff2
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-11-02 17:20:18 +0000

    basic player health
    player health and health bar.

---
+++ AIsDefence/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private int _maxHealth = 100;
+    [SerializeField]
+    private Slider _healthBar;
+
+    public int Health = 100;
+
+    private void Start()
+    {
+        _healthBar.maxValue = _maxHealth;
+        Respawn();
+    }
+
+    private void OnEnable()
+    {
+        Health = _maxHealth;
+        _healthBar.value = Health;
+    }
+
+    public void Respawn()
+    {
+        Health = _maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health = Health - damage;
+        _healthBar.value = Health;
+
+        if (Health <= 0)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        gameObject.SetActive(false);
+    }
+}
