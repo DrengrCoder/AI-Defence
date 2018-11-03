3903685c27665b48fa09930530f5f22871e06910
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-11-03 14:51:16 +0000

    proper controls
    Could do with a camera lock.

--- AIsDefence/Assets/Scripts/Player/PlayerController.cs
+++ AIsDefence/Assets/Scripts/Player/PlayerController.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float Speed = 10.0f;
-    public float RotateSpeed = 150.0f;
+    public float MouseSensitivity = 150.0f;
+
+    [SerializeField]
+    private Camera _cam;
+
+    private void OnEnable()
+    {
+        Cursor.visible = false;
+    }
+
+    private void OnDisable()
+    {
+        Cursor.visible = true;
+    }
 
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * RotateSpeed;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        float horizontal = Input.GetAxis("Mouse X") * MouseSensitivity;
+        float vertical = -Input.GetAxis("Mouse Y") * MouseSensitivity;
+
+        transform.Rotate(0, horizontal, 0);
+        _cam.transform.Rotate(vertical, 0, 0);
+
+        transform.Translate(x, 0, z);
+
+        if (Cursor.visible == true)
+        {
+            Cursor.visible = false;
+        }
     }
 }
