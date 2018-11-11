6e5600cbb6b78a0e2ba71f4e12e245d9c25d6920
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-11-11 01:25:09 +0000

    new models
    Really need to fix the movement glitch caused by navmesh

--- AIsDefence/Assets/Scripts/Enemies/RangedEnemy.cs
+++ AIsDefence/Assets/Scripts/Enemies/RangedEnemy.cs
@@ -6,6 +6,8 @@ using UnityEngine.AI;
 public class RangedEnemy : Enemy {
 
     public EnemyProjectilePool BulletPool;
+    [SerializeField]
+    private GameObject _bulletSpawn;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -31,7 +33,7 @@ public class RangedEnemy : Enemy {
             if (target.GetComponent<Objective>())
             {
                 GameObject bullet = BulletPool.GetBullet(Damage);
-                bullet.transform.position = this.transform.position;
+                bullet.transform.position = _bulletSpawn.transform.position;
                 bullet.SetActive(true);
                 bullet.GetComponent<RangedEnemyProjectile>().Shoot(target.transform.position);
                 CanAttack = false;
